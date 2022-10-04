7ff108d85e52288252e23a8badb7decb857f5d35
STARRY GEUST 2 <>
2022-10-04 18:17:10 +0900

    10/4 slack参照

--- components/common/Arrow.jsx
+++ components/common/Arrow.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import arrowSheet from '../../styles/common/arrow.module.scss'
 
-export default function Arrow() {
+export default function Arrow({scrollTop}) {
     return (
             <div className={arrowSheet.container}>
-                <a href='/' className={arrowSheet.arrow}/>
+                <a href='#' className={arrowSheet.arrow} onClick={scrollTop}/>
             </div>
     );
 }
